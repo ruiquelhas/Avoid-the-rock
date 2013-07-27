61a69c22ec932e0c132e5c4031fbbb5b3b0bd9ed
Rui Quelhas <rfpquelhas@gmail.com>
2013-07-27 20:13:04 +0100

    add test to match the canvas elements dimension
    * extract the cheerio context loading to the beforeEach hook
    * add test to ensure the canvas elements have the same dimension

--- test/integration/screenTestCase.js
+++ test/integration/screenTestCase.js
@@ -3,11 +3,14 @@ var expect = require('expect.js');
 var Primus = require('primus');
 
 module.exports.run = function () {
-  var context;
+  var context, $;
 
   beforeEach(function (done) {
     context = this.client;
-    context.visit('/screen', done);
+    context.visit('/screen', function () {
+      $ = cheerio.load(context.html('#screen'));
+      done();
+    });
   });
 
   describe('test the availability of the screen page', function () {
@@ -31,8 +34,13 @@ module.exports.run = function () {
 
     describe('test the essential page contents', function () {
       it('the page should have two different canvas', function () {
-        $ = cheerio.load(context.html('#screen'));
         expect($('canvas').length).to.be(2);
+      });
+
+      it('the canvases should have the same dimensions', function () {
+        var $playerCanvas = $('#player'), $actionCanvas = $('#action');
+        expect($playerCanvas.attr('width')).to.be($actionCanvas.attr('width'));
+        expect($playerCanvas.attr('height')).to.be($actionCanvas.attr('height'));
       });
     });
   });
